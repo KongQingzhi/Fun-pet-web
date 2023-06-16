23b263140a191eeb675d5d2e319b2e3ab46402c7
孔庆志 <1428390162@qq.com>
2023-06-16 21:38:41 +0800

    feat:新增NoData组件

---
+++ frontend/src/components/NoData.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="relative space-y-4 py-8 overflow-hidden">
+    <p class="text-center text-4xl font-bold">No Data</p>
+    <p class="text-center text-lg">暂无数据</p>
+    <div class="absolute top-0 -left-12 -z-10 bg-sky-400 -rotate-45 w-52">11</div>
+  </div>
+</template>
+
+<script lang="ts" setup></script>
